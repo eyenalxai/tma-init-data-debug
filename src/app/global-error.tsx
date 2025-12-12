b2261f29947354c92a15d0888d49b5030fcdb1e0
Sax Dhiut <mail@iyxeyl.me>
2025-12-12 15:15:27 +0400

    feat: add GlobalErrorPage component for improved error display
    - Introduced a new `GlobalErrorPage` component to handle and display error messages in a user-friendly manner.
    - Utilized the `Alert` component to present error information, enhancing user experience during error occurrences.

---
+++ src/app/global-error.tsx
@@ -0,0 +1,26 @@
+"use client"
+import { CircleAlertIcon } from "lucide-react"
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+import { cn } from "@/lib/utils"
+
+type GlobalErrorPageProps = {
+	error: Error
+}
+
+export default function GlobalErrorPage({ error }: GlobalErrorPageProps) {
+	return (
+		<html lang="en">
+			<body className={cn("font-sans", "antialiased")}>
+				<Alert variant="error">
+					<CircleAlertIcon />
+					<AlertTitle>Heads up!</AlertTitle>
+					<AlertDescription>
+						{error instanceof Error
+							? error.message
+							: "An unknown error occurred"}
+					</AlertDescription>
+				</Alert>
+			</body>
+		</html>
+	)
+}
