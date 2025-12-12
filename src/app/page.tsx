879e559c4ff643479d2d4c7096a7d4070f7c0f45
Sax Dhiut <mail@iyxeyl.me>
2025-12-12 14:07:55 +0400

    refactor: update page layout and enhance provider structure
    - Added padding to the main container in `page.tsx` for improved layout.
    - Wrapped children in `TelegramSDKProvider` within `ClientRoot` in `providers.tsx` to integrate Telegram SDK functionality.
    - Enhanced overall component structure for better organization and maintainability.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { ServerInitData } from "@/components/server-init-data"
 
 export default function Page() {
 	return (
-		<main className="container mx-auto max-w-md mt-12 space-y-8">
+		<main className="container mx-auto max-w-md mt-12 space-y-8 px-4">
 			<ClientInitData />
 			<ServerInitData />
 		</main>
--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -3,13 +3,16 @@
 import type { ThemeProviderProps } from "next-themes"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { ClientRoot } from "@/components/client-root"
+import { TelegramSDKProvider } from "@/components/providers/telegram-sdk"
 import { TRPCReactProvider } from "@/components/providers/trpc-provider"
 
 export function Providers({ children, ...props }: ThemeProviderProps) {
 	return (
 		<NextThemesProvider {...props}>
 			<TRPCReactProvider>
-				<ClientRoot>{children}</ClientRoot>
+				<ClientRoot>
+					<TelegramSDKProvider>{children}</TelegramSDKProvider>
+				</ClientRoot>
 			</TRPCReactProvider>
 		</NextThemesProvider>
 	)
